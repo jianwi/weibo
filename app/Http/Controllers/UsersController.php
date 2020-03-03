fb76b7fca427145765a98ed8aba0fb2a5ec20fd8
dujianjun <v@jianwi.cn>
2020-03-03 18:14:08 +0800

    关注和粉丝列表页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,27 +10,29 @@ class UsersController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth',[
-            'except' => ['show','create', 'store','index','confirmEmail']
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store', 'index', 'confirmEmail']
         ]);
 
-        $this->middleware('guest',[
-            'only'=>['create']
+        $this->middleware('guest', [
+            'only' => ['create']
         ]);
     }
 
     public function index()
     {
         $users = User::paginate(10);
-        return view('users.index',compact('users'));
+        return view('users.index', compact('users'));
     }
+
     public function show(User $user)
     {
         $statuses = $user->statuses()
-            ->orderBy('created_at','desc')
+            ->orderBy('created_at', 'desc')
             ->paginate(10);
-        return view('users.show',compact('user','statuses'));
+        return view('users.show', compact('user', 'statuses'));
     }
+
     public function create()
     {
         return view('users.create');
@@ -38,7 +40,7 @@ class UsersController extends Controller
 
     public function store(Request $request)
     {
-        $this->validate($request,[
+        $this->validate($request, [
             'name' => 'required|unique:users|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6|max:30'
@@ -50,40 +52,41 @@ class UsersController extends Controller
             'password' => bcrypt($request->password)
         ]);
         $this->sendEmailConfirmationTo($user);
-        session()->flash('success','验证邮件已发送到您的邮箱，请注意查收。');
+        session()->flash('success', '验证邮件已发送到您的邮箱，请注意查收。');
         return redirect('/');
     }
 
     public function edit(User $user)
     {
-        $this->authorize('update',$user);
-        return view('users.edit',compact('user'));
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
     }
-    public function update(User $user,Request $request)
+
+    public function update(User $user, Request $request)
     {
-        $this->authorize('update',$user);
-        $this->validate($request,[
+        $this->authorize('update', $user);
+        $this->validate($request, [
             'name' => 'required|max:50',
             'password' => 'nullable|confirmed|min:6'
         ]);
         $data = [];
         $data['name'] = $request->name;
-        if ($request->password){
+        if ($request->password) {
             $data['password'] = $request->password;
         }
         $user->update($data);
 
-        session()->flash('success','个人资料更新成功');
+        session()->flash('success', '个人资料更新成功');
 
-        return redirect()->route('users.show',$user);
+        return redirect()->route('users.show', $user);
     }
 
     public function destroy(User $user)
     {
-        $this->authorize('destroy',$user);
+        $this->authorize('destroy', $user);
         $name = $user->name;
         $user->delete();
-        session()->flash('info','成功删除用户! '.$name);
+        session()->flash('info', '成功删除用户! ' . $name);
         return back();
     }
 
@@ -95,19 +98,33 @@ class UsersController extends Controller
         $name = 'Summer';
         $to = $user->email;
         $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
-        Mail::send($view,$data,function ($message)use ($from,$name,$to,$subject){
+        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
             $message->to($to)->subject($subject);
         });
     }
 
     public function confirmEmail($token)
     {
-        $user = User::where('activation_token',$token)->firstOrFail();
+        $user = User::where('activation_token', $token)->firstOrFail();
         $user->activated = true;
         $user->activation_token = null;
         $user->save();
         \Auth::login($user);
-        session()->flash('success','恭喜，激活成功！');
-        return redirect()->route('users.show',$user);
+        session()->flash('success', '恭喜，激活成功！');
+        return redirect()->route('users.show', $user);
     }
+
+    public function followings(User $user)
+    {
+        $users = $user->followings()->paginate(30);
+        $title = $user->name . '关注的人';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+    public function followers(User $user)
+    {
+        $users = $user->followers()->paginate(30);
+        $title = $user->name . '的粉丝';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
 }
